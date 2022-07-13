1810b6b7c0ff5ed4f3f20298aaf583c2c0b23d1e
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-07-13 23:50:50 +0530

    Add files via upload

---
+++ PRINT.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("enter number a");
+scanf("%d",&a);
+printf("enter number b");
+scanf("%d",&b);
+printf("numbers is %d %d",a,b);
+getch();
+return 0;
+}
