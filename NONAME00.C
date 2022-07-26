a5fbd4d86014ee0148a47b25fea10f8038fe08f3
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-07-27 00:02:08 +0530

    Update NONAME00.C

--- NONAME00.C
+++ NONAME00.C
@@ -1,11 +1,16 @@
+/* size of data types */
 #include<stdio.h>
 #include<conio.h>
 int main()
 {
+int x;
+char y;  
 short a;
 long b;
 long long c;
 long double d;
+printf("size of int = %d byte \n",sizeof(x));
+printf("size of char = %d byte \n ",sizeof(y));
 printf("side of short = %d byte\n",sizeof(a));
 printf("size of long = %d bytes \n",sizeof(b));
 printf("size of long long = %d bytes \n",sizeof(c));
