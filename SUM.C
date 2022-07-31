2543cdd6cf4d1f40f230e17704594bf307d53419
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-07-31 22:45:24 +0530

    Add files via upload

---
+++ SUM.C
@@ -0,0 +1,16 @@
+// addition of two numbers//
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+clrscr();
+int a,b,x;
+printf(" enter the first number");
+scanf("%d",&a);
+printf("enter the second number");
+scanf("%d",&b);
+x=a+b;
+printf("the sum of two numbers is = %d",x);
+getch();
+return 0;
+}
