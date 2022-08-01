8fb288586cade54f7bc97fb27e5a31b3da3a74c4
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-01 23:35:56 +0530

    Create division.c

---
+++ division.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+clrscr();
+printf("enter the bigger value of divisor ");
+scanf("%d",&a);
+printf("enter the smaller value of divedent ");
+scanf("%d",&b);
+c=a/b;
+printf("the division is %d",c);
+getch();
+return 0;
