22e9814a64cb15dd9664234e0649ef3fbd9a48d6
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-01 23:31:16 +0530

    Add files via upload

---
+++ MULTIPLI.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+clrscr();
+int a,b,c;
+printf("enter the first value ");
+scanf("%d",&a);
+printf("enter the second value ");
+scanf("%d",&b);
+c=a*b;
+printf("the multiplication is %d",c);
+getch();
+return 0;
+}
