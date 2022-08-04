f92081e683681ce3eb0dae43bd82cabb08f4630f
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-04 23:32:27 +0530

    Create cube.c

---
+++ cube.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+clrscr();
+int a,c;
+printf("enter the number");
+scanf("%d",&a);
+c=a*a*a;
+printf("the cube is %d",c);
+getch();
+return 0;
+}
