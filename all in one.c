2202e72971c7f3f99accffec8fd2adbf42f289b3
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-05 23:39:48 +0530

    Create all in one.c

---
+++ all in one.c
@@ -0,0 +1,22 @@
+// sum,sub,mul,div all in one program //
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+clrscr();
+int a,b,x,s,m.d;
+printf(" enter the first number");
+scanf("%d",&a);
+printf("enter the second number");
+scanf("%d",&b);
+x=a+b;
+s=a-b;
+m=a*b;
+d=a/b;
+printf("the sum of two numbers is = %d",x);
+printf("the subtraction of two numbers is = %d",s);
+printf("the multiplication of two numbers is = %d",m);
+printf("the division of two numbers is = %d",d);
+getch();
+return 0;
+}
