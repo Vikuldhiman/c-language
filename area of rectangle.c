f0df2584088255b51d88f070237e682d82ea7b18
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-09 22:47:15 +0530

    Create area of rectangle.c

---
+++ area of rectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   clrscr();
+  int ar,l,b;
+  cout<<" Enter the value of lenght and breadth";
+  cin>>l>>b;
+  ar=l*b;
+  cout<<" the area of rectangle is = "<<ar;
+  getch();
+  return0;
+}
