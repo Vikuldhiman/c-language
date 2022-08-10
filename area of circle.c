5953ac4e58d123d2c59ea947da0c1866a7a42bde
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-10 23:55:04 +0530

    Create area of circle.c

---
+++ area of circle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   clrscr();
+  int ar,r;
+  cout<<" Enter the value  of radius";
+  cin>>r;
+  ar=3.14*r*r;
+  cout<<" the area of circle is = "<<ar;
+  getch();
+  return0;
+}
