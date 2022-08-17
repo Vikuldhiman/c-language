ccfc0b82f01b7f210de21f8d2b1d49ec3b9c6425
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-17 23:50:04 +0530

    Create greater number .c

---
+++ greater number .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+  int main()
+  {
+     int a,b;
+    printf("enter the first number");
+    scanf("%d",&a);
+    printf("enter the second number");
+    scanf("%d",&b);
+    if(a>b)
+    {
+      printf("the greater number is %d",a);
+    }
+    else
+    {
+      printf("the greater number is %d",b);
+    }
+    getch();
+    retrun 0;
+  }
