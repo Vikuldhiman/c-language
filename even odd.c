b3f7fc628c18d684e3bab226b48fe72f29aa079a
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-20 21:59:18 +0530

    Create even odd.c

---
+++ even odd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+  int main()
+  {
+     int a,b;
+    printf("enter the first number");
+    scanf("%d",&a);
+    printf("enter the second number");
+    scanf("%d",&b);
+    if(a%2==0)
+    {
+      printf("the number %d is even",a);
+    }
+    else
+    {
+      printf("the number %d is odd",b);
+    }
+    getch();
+    retrun 0;
+  }
