d53f95b82faf6fa93aa525faf9676bc158b16a17
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-21 23:17:40 +0530

    Create strcmp function .c

---
+++ strcmp function .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+  char str[]="vikhul";
+  char stre[]="vikul";
+  int value = strcmp(str,stre);
+  printf("%s",value);
+  getch();
+}
