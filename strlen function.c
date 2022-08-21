895da1efe895530a7301789958a6064885c7c787
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-21 23:21:32 +0530

    Create strlen function.c

---
+++ strlen function.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+  char str[]="My Name Is Vikul Dhiman";
+  int n= strlen(str);
+  printf("the lenght of the sentence is %d",str);
+  getch();
+}
