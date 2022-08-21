a1441d775f72686d3d283b7e90f834bd517fd0e0
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-21 23:10:48 +0530

    Create strcat function.c

---
+++ strcat function.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+  char str[]="hello";
+  char stre[]="vikul";
+  strcat(str,stre);
+  printf("%s",str);
+  getch();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+,.
+  
+  
+  <<
