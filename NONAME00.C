bbc4c0f6440b53ab5b28acefe5479de8d16fa407
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-09-02 22:01:59 +0530

    Add files via upload

--- NONAME00.C
+++ NONAME00.C
@@ -1,20 +1,21 @@
-/* size of data types */
 #include<stdio.h>
 #include<conio.h>
 int main()
 {
-int x;
-char y;  
-short a;
-long b;
-long long c;
-long double d;
-printf("size of int = %d byte \n",sizeof(x));
-printf("size of char = %d byte \n ",sizeof(y));
-printf("side of short = %d byte\n",sizeof(a));
-printf("size of long = %d bytes \n",sizeof(b));
-printf("size of long long = %d bytes \n",sizeof(c));
-printf("size of long double = %d byte",sizeof(d));
+clrscr();
+int n,rem,sum=0;
+printf("enter four digit number");
+scanf("%d"&n);
+while(n>0)
+{
+ rem=n%10;
+if(rem%2==0)
+{
+ sum=sum+rem;
+}
+ n=n/10;
+}
+printf("sum= %d",sum);
 getch();
 return 0;
 }
