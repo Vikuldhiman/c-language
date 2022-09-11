5a9230e60e27c26eb7a1fa799a028f4b7487b60a
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-09-11 23:14:59 +0530

    Create dowhile.c

---
+++ dowhile.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a;
+  printf("enter a  two digit number");
+  scanf("%d",&a);
+  do
+  {
+    printf("hello");
+  }
+  while(a<=99)
+  {
+  printf("how are you");
+  }
+  getch();
+  ;
+  
